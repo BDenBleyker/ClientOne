37a78b53994ce3742811ce59e9da85ead19f994b
Bennett DenBleyker <bennett.denbleyker@apastudents.org>
2017-03-30 21:09:08 -0600

    Update SimpleMessageStore.java

--- src/clienttwo/SimpleMessageStore.java
+++ src/clienttwo/SimpleMessageStore.java
@@ -10,16 +10,20 @@ import java.util.List;
  */
 public class SimpleMessageStore implements MessageStore {
 
-    
+    List<Message> messages = new ArrayList();
     
     @Override
     public void store(String username, String message) {
-        
+        messages.add (new Message(username, message));
     }
 
     @Override
     public void dumpAll() {
-        
+        String formatCode = "%-15s %-15s %-15s";
+        System.out.println(String.format(formatCode, "Date", "From", "Message"));
+        for (Message msg : messages) {
+            System.out.println(msg.toFormattedString(formatCode));
+        }
     }
 
 }
